c9153fc13fa0f492214796d9a6be3e67c2bc5184
Athif Shaffy <athif.shaffy@gmail.com>
2022-12-09 09:32:06 -0500

    Updated register page

--- CryptoCrash/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ CryptoCrash/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,6 +71,17 @@ namespace CryptoCrash.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [Required]
+            [StringLength(255, ErrorMessage = "The first name should have a maximum of 255 characters")]
+            [Display(Name="Firstname")]
+            public string FirstName { get; set; }
+
+
+            [Required]
+            [StringLength(255, ErrorMessage = "The last name should have a maximum of 255 characters")]
+            [Display(Name = "Lastname")]
+            public string LastName { get; set; }
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -114,6 +125,8 @@ namespace CryptoCrash.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
+                user.FirstName = Input.FirstName;
+                user.LastName = Input.LastName;
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
