b5f09733d79d1280dc654978e0abe931454d700f
Athif Shaffy <athif.shaffy@gmail.com>
2022-12-09 09:25:17 -0500

    Added migrations, fixed db connection issue on windows sqlserver

--- CryptoCrash/Areas/Identity/Data/ApplicationDbContext.cs
+++ CryptoCrash/Areas/Identity/Data/ApplicationDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace CryptoCrash.Areas.Identity.Data;
 
@@ -18,5 +19,16 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
         // Customize the ASP.NET Identity model and override the defaults if needed.
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
+
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+    }
+}
+
+public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.Property(u => u.FirstName).HasMaxLength(255);
+        builder.Property(u => u.LastName).HasMaxLength(255);
     }
 }
--- CryptoCrash/Program.cs
+++ CryptoCrash/Program.cs
@@ -35,4 +35,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapRazorPages();
+
 app.Run();
