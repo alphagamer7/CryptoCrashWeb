8f7a07ba1c340bf1a66b40d7f6b2753c219f23b0
preetyabhishek <theshivamsharma01@gmail.com>
2022-12-09 16:54:18 -0500

    Updated Cryptocurrency model

--- CryptoCrashLogic/CryptCurrency.cs
+++ CryptoCrashLogic/CryptCurrency.cs
@@ -3,13 +3,21 @@ namespace CryptoCrashLogic
 {
     public class CryptCurrency
     {
-        public string CurrencyCode { get; set; }
-        public double CurrencyValue { get; set; }
+        public string Asset_id_quote { get; set; }
+        public double Asset_id_base { get; set; }
+        public SrcData Src_side_quote { get; set; }
 
         public CryptCurrency()
         {
 
         }
+    }
+
+    public class SrcData
+    {
+        public string Asset { get; set; }
+        public double Rate { get; set; }
+        public double Volume { get; set; }
     }
 }
 
