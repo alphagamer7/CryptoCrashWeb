cfcb1a1ad1ef8c4e3db0830d276e62fbc172a2c4
Athif Shaffy <athif.shaffy@gmail.com>
2022-12-09 23:33:00 -0500

    Added API tests

--- CryptoCrashTest/ApiTest.cs
+++ CryptoCrashTest/ApiTest.cs
@@ -3,8 +3,54 @@
 [TestClass]
 public class UnitTest1
 {
-    [TestMethod]
-    public void TestMethod1()
-    {
-    }
+    public class MockedResponse
+        {
+            public string? testString { get; set; }
+        }
+
+        [TestMethod]
+        public void TestApiDeserilization()
+        {
+            NewsApiClient<MockedResponse> client = new();
+            var json = @"{
+                          'data': {
+                            'upcoming': [
+                              {
+                                'testString': 'test1'
+                              },
+                              {
+                                'testString': 'test2'
+                              },
+                                ]
+                            }
+                         }";
+            var res = client.CreateNewsFromJson(json);
+            Assert.IsNotNull(res);
+            Assert.AreEqual(2, res.Count);
+            Assert.AreEqual("test1", res[0].testString);
+            Assert.AreEqual("test2", res[1].testString);
+        }
+
+         [TestMethod]
+        public void TestApiResponse()
+        {
+            MovieApiClient<MockedResponse> client = new();
+            var json = @"{
+                          'data': {
+                            'upcoming': [
+                              {
+                                'testString': 'test1'
+                              },
+                              {
+                                'testString': 'test2'
+                              },
+                                ]
+                            }
+                         }";
+            var res = client.CreateNewsFromJson(json);
+            Assert.IsNotNull(res);
+            Assert.AreEqual(2, res.Count);
+            Assert.AreEqual("test1", res[0].testString);
+            Assert.AreEqual("test2", res[1].testString);
+        }
 }
