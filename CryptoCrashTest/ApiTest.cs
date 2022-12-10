416aff7b6deba89d10442797502358e2f46ee0b6
Athif Shaffy <athif.shaffy@gmail.com>
2022-12-10 01:25:10 -0500

    Added more test cases

--- CryptoCrashTest/ApiTest.cs
+++ CryptoCrashTest/ApiTest.cs
@@ -30,27 +30,11 @@ public class UnitTest1
             Assert.AreEqual("test1", res[0].testString);
             Assert.AreEqual("test2", res[1].testString);
         }
-
-         [TestMethod]
-        public void TestApiResponse()
-        {
-            MovieApiClient<MockedResponse> client = new();
-            var json = @"{
-                          'data': {
-                            'upcoming': [
-                              {
-                                'testString': 'test1'
-                              },
-                              {
-                                'testString': 'test2'
-                              },
-                                ]
-                            }
-                         }";
-            var res = client.CreateNewsFromJson(json);
-            Assert.IsNotNull(res);
-            Assert.AreEqual(2, res.Count);
-            Assert.AreEqual("test1", res[0].testString);
-            Assert.AreEqual("test2", res[1].testString);
-        }
+//  [TestMethod]
+//          public async Task Check_For_Empty_TimeSheetList_TestAsync()
+//          {
+//              List<TimeSheet> timeSheets = await timesheetViewModel.GetTimeSheets();
+//              Assert.IsTrue(timeSheets.Count == 0);
+//          }
+   
 }
--- CryptoCrashTest/UnitTestLogic.cs
+++ CryptoCrashTest/UnitTestLogic.cs
@@ -1,13 +1,29 @@
 ﻿using System;
+using CryptoCrashLogic.Services;
+
 namespace CryptoCrashTest
 {
     [TestClass]
     public class UnitTestLogic
     {
+        NewsApiClient newApiClient;
 
         public UnitTestLogic()
         {
         }
+
+        [TestInitialize]
+        public void Instantiate()
+        {
+            newApiClient = new NewsApiClient();
+        }
+
+        [TestMethod]
+        public void NewsClientClassIsInstantiable()
+        {
+            Assert.IsNotNull(newApiClient);
+        }
+
     }
 }
 
