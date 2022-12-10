a8ae3bbfc79d386f48672ccdf4bd75216fa03814
Athif Shaffy <athif.shaffy@gmail.com>
2022-12-10 21:23:17 +0530

    Updates for news client api
    News client api loads only after we do a normal load on browser, because its a free api, so we added a temp string in case there is no response from news api

--- CryptoCrashLogic/Services/NewsApiClient.cs
+++ CryptoCrashLogic/Services/NewsApiClient.cs
@@ -9,8 +9,7 @@ namespace CryptoCrashLogic.Services
     public class NewsApiClient<T>
     {
         private readonly HttpClient client;
-        private readonly string MOVIE_API_URL;
-        private readonly string API_KEY;
+   
 
         public NewsApiClient()
         {
@@ -18,43 +17,28 @@ namespace CryptoCrashLogic.Services
         }
 
         private WebClientService clientService = new WebClientService();
-        public static string newsUrl = 
-            "https://newsapi.org/v2/everything?q=cryptocurrency&apiKey=D08d6b3c579542f0abbae1f6a3c3949a";
+        public static string newsUrl =
+            "https://newsapi.org/v2/everything?q=cryptocurrency&apiKey=D08d6b3c579542f0abbae1f6a3c3949a&page=1";
 
 
         public async Task<List<T>> GetNews()
         {
-
-
             HttpResponseMessage response = await client.GetAsync(newsUrl);
-            
-            var body =  response.IsSuccessStatusCode ? await response.Content.ReadAsStringAsync() : null; // since sometimes api timeouts
-   
-            
-                return CreateNewsFromJson(body);
-        }
 
-        public async Task<List<T>> GetNewsItems()
-        {
-            var json = await clientService.GetAsync(newsUrl);
-            return CreateNewsFromJson(json);
+            var json2 = "{\"status\":\"ok\",\"totalResults\":16214,\"articles\":[{\"source\":{\"id\":\"bbc-news\",\"name\":\"BBC News\"},\"urlToImage\":\"https://ichef.bbci.co.uk/news/1024/branded_news/173B1/production/_127735159_mediaitem127735158.jpg\",\"author\":\"https://www.facebook.com/bbcnews\",\"publishedAt\":\"2022-11-22T07:56:41Z\",\"title\":\"Estonian duo accused of $575m cryptocurrency scam\",\"description\":\"The men are accused of buying luxury cars after getting people to buy into fraudulent crypto mining.\"}]}";
+
+            var body =  response.IsSuccessStatusCode ? await response.Content.ReadAsStringAsync() : json2; // since sometimes api timeouts
+   
+            return CreateNewsFromJson(body);
         }
 
         public List<T> CreateNewsFromJson(string json)
         {
        
-            var resVal = JsonConvert.DeserializeObject<NewsResponse>(json);
-            Console.WriteLine(resVal.articles);
             var res = JsonConvert.DeserializeObject<Response<T>>(json);
             return res?.articles!;
         }
 
-    }
-
-    public class NewsResponse
-    {
-        public List<News> articles { get; set; }
-        public string status { get; set; }
     }
 
     
