5c15e39a4c50d537e25ad236936d8e072787dfc2
Shivam Sharma <theshivamsharma01@gmail.com>
2022-12-17 18:06:01 +0530

    Added data annotation for news model
    Missed to commit the news model data annotations.

--- CryptoCrashLogic/Model/News.cs
+++ CryptoCrashLogic/Model/News.cs
@@ -1,16 +1,28 @@
 
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CryptoCrashLogic.Model
 {
     public class News
     {
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
         public string Title { get; set; }
+
+        [Required]
         public string Author { get; set; }
+
+        [Required]
         public string Description { get; set; }
+
+        [Required]
         public string urlToImage { get; set; }
+
+        [Required]
         public string PublishedAt { get; set; }
-        public string Content { get; set; }
 
     }
 }
